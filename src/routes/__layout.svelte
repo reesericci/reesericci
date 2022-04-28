ca101b88c01009b8d5bbe3e22695497bdb084b9e
reesericci <me@reeseric.ci>
2022-04-27 22:28:31 -0500

    favicon and title

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,3 +1,8 @@
+<svelte:head>
+	<title>reeseric.ci</title>
+	<link rel="icon" type="image/png" href="/src/lib/assets/avatar.png">
+</svelte:head>
+
 <a href="/"><h1>reeseric.ci</h1></a>
 
 <slot></slot>
