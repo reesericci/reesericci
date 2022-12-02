c0a98c9c7af623f64634941ac79c2f97187a8417
reesericci <me@reeseric.ci>
2022-12-01 21:27:47 -0600

    whoopsie

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
-
 import mdx from "@astrojs/mdx";
+import webfinger from 'astro-webfinger'
 
 // https://astro.build/config
 export default defineConfig({
